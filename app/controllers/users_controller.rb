ff216a22ef9268a1e7be7862db10f321a9d4a0ab
Arthur <ajautz@gmail.com>
2018-09-24 01:30:17 +0000

    Issues with exercises at the end of 7.1.4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,9 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    #@user = User.find(params[:created_at])
-    #@user = User.find(params[:updated_at])
-    
   end 
   
-  
   def new
-    #debugger
   end
   
 end
