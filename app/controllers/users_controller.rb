b68f3acedf7fa00579de5b61500cd39ca427bc38
Arthur <ajautz@gmail.com>
2018-09-24 01:05:04 +0000

    Add gravitas with Gravatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
+    #@user = User.find(params[:created_at])
+    #@user = User.find(params[:updated_at])
     
+  end 
+  
+  
+  def new
+    #debugger
   end
   
 end
