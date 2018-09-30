46f2a643ada2a7c20e3b21c754498e0b79d51d67
Arthur <ajautz@gmail.com>
2018-09-30 02:55:08 +0000

    Fix route so that unsuccessful signups redirect back to the same signup page

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,16 +1,20 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
   test "invalid signup information" do
     get signup_path #why does this produce an error? undefined?
+    
+    assert_select 'form[action="/signup"]'
+    
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name: "", email: "user@invalid", password: "foo", password_confirmation: "bar" } }
+      post signup_path, params: { user: { name: "", email: "user@invalid", password: "foo", password_confirmation: "bar" } }
     end
     assert_template 'users/new'
-    #assert_select 'name#<name>'
-    #assert_select 'name.<form-control>'
+    
+    assert_select 'div#error_explanation' 
+    assert_select 'div.field_with_errors'
+    
+
   end 
 end
