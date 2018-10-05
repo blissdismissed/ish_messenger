2566b3c6acbf239087d58a835af323b69a0420d9
Arthur <ajautz@gmail.com>
2018-10-05 23:00:57 +0000

    Add new view for the login, add routes for login

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,13 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get '/help', to: 'static_pages#help'
   get '/signup', to: 'users#new'
+  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  
   post '/signup',  to: 'users#create'
+  
   resources :users
   
 end
