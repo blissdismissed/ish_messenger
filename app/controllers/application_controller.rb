c818b33d1565ca3600c543b6bcfd4fa342fc4f21
Arthur <ajautz@gmail.com>
2018-10-07 22:35:36 +0000

    Add log_in and current_user methods using session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include SessionsHelper
   
-  def hello
-    render html: "hello, world! Not."
-  end 
+  #def hello
+  #  render html: "hello, world! Not."
+  #end 
   
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,16 @@
 module SessionsHelper
+  
+  # Logs in the given user.
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+  
+  # Returns the current user if logged in
+  def current_user
+    if sessions[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end 
+  end 
+      
+  
 end
