33e65f865aeaac4830b9a2e5b9388f0d45d32135
Arthur <ajautz@gmail.com>
2018-10-14 20:16:58 +0000

    Fix small bugs of double logouts

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,7 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, length: { minimum: 6 }, presence: true
   
-    # Returns the hash digest of the given string.
-
+  # Returns the hash digest of the given string.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
@@ -31,5 +30,17 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
     #p digest(remember_token)
   end 
+  
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end 
+  
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
+  end 
+  
   
 end
